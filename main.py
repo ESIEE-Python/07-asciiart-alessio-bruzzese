14500fa383325d83e746d5b193eebb84e0938d90
alessio-bruzzese <alessio.bruzzese@edu.esiee.fr>
2024-11-28 09:02:35 +0000

    programme ASCII encodage

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#### Imports et définition des variables globales
-
+"""Programme qui retourne le nombre d'occurence d'un caractère dans une chaine de
+caractère"""
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +14,23 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    # votre code ici
+    result = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            result.append((s[i - 1], count))
+            count = 1
+    result.append((s[-1], count))
+    return result
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne 
+    de caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +38,31 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
+    # Cas de base
+    if not s:
+        return []
 
-    # cas de base
+    result = []
+    count = 1
     # recherche nombre de caractères identiques au premier
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            result.append((s[i - 1], count))
+            count = 1
     # appel récursif
+    result.append((s[-1], count))  # Ajouter le dernier groupe
+    return result
 
-    return []
-    
 
 #### Fonction principale
 
-
 def main():
+    """fonction principale permettant l'execution du programme"""
+
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
